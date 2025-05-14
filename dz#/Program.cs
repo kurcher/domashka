6cd158e082867a2a4b5586b4761379a9b3d8cd1f
kurcher <kurch2950@gmail.com>
2025-05-14 17:05:40 +0300

    dz

---
+++ dz#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using CharMatrixLibrary;
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+        Console.Write("Введіть розмір квадратної матриці: ");
+        int n = int.Parse(Console.ReadLine());
+
+        CharMatrix matrix = new CharMatrix(n);
+        matrix.PrintMatrix();
+
+        Console.WriteLine("\nГоловна діагональ: " + matrix[0]);
+        Console.WriteLine("Побічна діагональ: " + matrix[1]);
+        Console.WriteLine("Кількість голосних: " + matrix.VowelCount);
+    }
+}
